fe423c75cd4ef6558318a81019772bffabf75c51
Russell Mull <russell@digitalartificer.com>
2016-10-31 16:04:35 -0700

    Add vfs layer

---
+++ src/reading_room/fs.clj
@@ -0,0 +1,121 @@
+(ns reading-room.fs
+  (:require [clojure.java.io :as io]
+            [clojure.spec :as s]
+            [clojure.string :as string]
+            [reading-room.zip :as zip]
+            [clojure.walk :as walk]
+            [reading-room.fs :as fs]))
+
+;;; A small VFS layer that knows how to reach inside zip files
+
+(s/def ::name string?)
+(s/def ::path string?)
+(s/def ::path-in-zip string?)
+(s/def ::children (s/map-of ::name ::file))
+
+(defmulti file-type ::type)
+
+(defmethod file-type :file [_]
+  (s/keys :req [::name ::path]))
+
+(defmethod file-type :directory [_]
+  (s/keys :req [::name ::path]))
+
+(defmethod file-type :zipped-file [_]
+  (s/keys :req [::name ::path ::path-in-zip]))
+
+(defmethod file-type :zipped-directory [_]
+  (s/keys :req [::name ::path ::path-in-zip ::children]))
+
+
+(s/def ::file (s/multi-spec file-type ::type))
+
+(defn- add-node-to-tree [root path node]
+  (if-not (seq path)
+    (assoc-in root [::children (::name node)] node)
+    (let [[name & names] path
+          child-node (get-in root [::children name]
+                             {::type :zipped-directory
+                              ::name name
+                              ::children {}})]
+      (assoc-in root [::children name]
+                (add-node-to-tree child-node names node)))))
+
+(defn- roll-up-zip-entry-paths [archive-path entry-paths]
+  (reduce (fn [tree path]
+            (let [split-path (string/split path #"/")
+                  new-file {::name (last split-path)
+                            ::path archive-path
+                            ::path-in-zip path
+                            ::type (if (string/ends-with? path "/")
+                                     :zipped-directory
+                                     :zipped-file)}]
+              (add-node-to-tree tree (drop-last split-path) new-file)))
+          {::name "<root>"
+           ::path archive-path
+           ::path-in-zip ""
+           ::type :zipped-directory
+           ::children {}}
+          entry-paths))
+
+(defn- load-zip-file [archive-path]
+  (roll-up-zip-entry-paths archive-path (zip/zip-entries archive-path)))
+
+(s/fdef file :args (s/cat :root-path ::path) :ret ::file)
+(defn file [root-path]
+  (let [f (io/file root-path)]
+   {::name (.getName f)
+    ::path (.getAbsolutePath f)
+    ::type (if (.isDirectory f)
+             :directory
+             :file)}))
+
+(s/fdef file? :args (s/cat :f ::file) :ret boolean?)
+(defn file? [f]
+  (= :file (::type f)))
+
+(s/fdef directory? :args (s/cat :f ::file) :ret boolean?)
+(defn directory? [f]
+  (= :directory (::type f)))
+
+(defn- is-zip-file? [path]
+  (string/ends-with? path ".zip"))
+
+(defn children [f]
+  (let [path (::path f)]
+    (case (::type f)
+      :file
+      (when (is-zip-file? path)
+        (vals (::children (load-zip-file path))))
+
+      :zipped-file
+      nil
+
+      :directory
+      (let [jf (io/file path)]
+        (map file (seq (.listFiles jf))))
+
+      :zipped-directory
+      (sort-by ::name (vals (::children f))))))
+
+(defn has-children? [f]
+  (let [path (::path f)]
+    (case (::type f)
+      :file (is-zip-file? path)
+      :zipped-file false
+      :directory true
+      :zipped-directory true
+      false)))
+
+(defn sort-key [f]
+  (let [key-fn (case (::type f)
+                 :file ::path
+                 :zipped-file ::path-in-zip
+                 :directory ::path
+                 :zipped-directory ::path-in-zip)]
+    (key-fn f)))
+
+(defn content-stream [f]
+  (case (::type f)
+    :file (io/input-stream (::path f))
+    :zipped-file (zip/zip-entry-stream (::path f) (::path-in-zip f))))
---
+++ test/reading_room/fs_test.clj
@@ -0,0 +1,68 @@
+(ns reading-room.fs-test
+  (:require [reading-room.fs :as fs]
+            [clojure.test :as t]
+            [clojure.set :as set]))
+
+;; helpers for zip nodes
+(defn zipped-dir [name children]
+  {::fs/type :zipped-directory
+   ::fs/name name
+   ::fs/children (->> children
+                      (map (juxt ::fs/name identity))
+                      (into {}))})
+
+(defn zipped-file [name]
+  {::fs/type :zipped-file
+   ::fs/name name})
+
+(t/deftest add-node-to-tree-test
+  (t/are [root path node out] (= out (#'fs/add-node-to-tree root path node))
+    (zipped-dir "root" []) [] (zipped-file "test.txt")
+    (zipped-dir "root" [(zipped-file "test.txt")])
+
+    (zipped-dir "root" []) ["a"] (zipped-file "test.txt")
+    (zipped-dir "root" [(zipped-dir "a" [(zipped-file "test.txt")])])
+
+    (zipped-dir "root" []) ["a" "b"] (zipped-file "test.txt")
+    (zipped-dir "root" [(zipped-dir "a" [(zipped-dir "b" [(zipped-file "test.txt")])])])
+
+    (zipped-dir "root" [(zipped-file "test.txt")]) ["a"] (zipped-file "test.txt")
+
+    (zipped-dir "root" [(zipped-file "test.txt")
+                        (zipped-dir "a" [(zipped-file "test.txt")])])))
+
+(t/deftest test-dir
+  (let [f (fs/file "test_resources/test_dir")]
+    (t/is (= #::fs {:name "test_dir"
+                    :type :directory}
+             (dissoc f ::fs/path)))
+
+    (t/is (= #::fs {:name "test.txt"
+                    :type :file}
+             (dissoc (first (fs/children f))
+                     ::fs/path)))
+
+    (t/is (= "This is the dir content.\n"
+             (-> (first (fs/children f))
+                 fs/content-stream
+                 slurp)))))
+(-> (fs/file "test_resources/test.zip")
+    fs/children
+    first
+    fs/children
+    first)
+
+(t/deftest test-zip
+  (let [f (fs/file "test_resources/test.zip")]
+    (t/is (= #:fs {:name "test.zip"
+                   :type :file})
+          (dissoc f ::fs/path))
+
+    (t/is (= "a" (-> f fs/children first ::fs/name)))
+    (t/is (= :zipped-directory (-> f fs/children first ::fs/type)))
+
+    (t/is (= "file.txt" (-> f fs/children first fs/children first ::fs/name)))
+    (t/is (= :zipped-file (-> f fs/children first fs/children first ::fs/type)))
+
+    (t/is (= "This is the zip content.\n"
+             (-> f fs/children first fs/children first fs/content-stream slurp)))))
